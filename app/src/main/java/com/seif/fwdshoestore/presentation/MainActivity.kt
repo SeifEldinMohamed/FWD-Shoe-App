566039adf4d3a47a382bc3c21864e43747bf5bfb
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2023-01-19 09:40:54 +0200

    LoginFragment.kt Design Added

--- app/src/main/java/com/seif/fwdshoestore/presentation/MainActivity.kt
+++ app/src/main/java/com/seif/fwdshoestore/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.seif.fwdshoestore
+package com.seif.fwdshoestore.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seif.fwdshoestore.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/seif/fwdshoestore/presentation/login/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.seif.fwdshoestore.presentation.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.seif.fwdshoestore.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+    lateinit var binding: FragmentLoginBinding
+    private lateinit var email: String
+    private lateinit var password: String
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogin.setOnClickListener {
+            getUserInput()
+            Timber.d("onViewCreated: $email - $password")
+        }
+        binding.btnRegister.setOnClickListener {
+            getUserInput()
+            Timber.d("onViewCreated: $email - $password")
+        }
+    }
+
+    private fun getUserInput() {
+        email = binding.etEmail.text.toString()
+        password = binding.etPassword.text.toString()
+    }
+}
